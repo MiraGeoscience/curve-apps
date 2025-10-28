71bafb3070032cdcd65c917390a20059991726d1
dominiquef <dominiquef@mirageoscience.com>
2025-10-28 10:49:14 -0700

    Fix warning in test

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -21,7 +21,7 @@ from curve_apps.utils import (
 
 
 def test_set_vertices_height(tmp_path):
-    ws = Workspace(tmp_path / "test.geoh5")
+    ws = Workspace.create(tmp_path / f"{__name__}.geoh5")
     vertices = np.c_[np.random.randn(10), np.random.randn(10), np.ones(10)]
     pts = Points.create(ws, vertices=vertices, name="my points")
     new_vertices = set_vertices_height(vertices[:, :2], pts)
